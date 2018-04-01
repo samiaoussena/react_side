1fa3db3ea5c8ec86f58c183b597b7e65e3bdd7e4
samiaoussena <samia.oussena@uwl.ac.uk>
2018-04-01 19:24:23 +0100

    First Commit

---
+++ src/containers/Template.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import NavDrawer from '../components/NavDrawer'
+import {Header, Main} from '../styled/Template'
+
+injectTapEventPlugin()
+
+class Template extends Component {
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <NavDrawer/>
+          <Header>
+            Octopus
+          </Header>
+          <Main>
+            {this.props.children}
+          </Main>
+        </div>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+export default Template
